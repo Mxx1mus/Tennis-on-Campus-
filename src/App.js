ae2d3cdefe3bd23aa22d31f0c5eb89eaeac6dedc
Maximus Austin <maximus@ucr-secure-36-10-13-194-166.wnet.ucr.edu>
2024-11-16 10:57:28 -0800

    Added design features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Greeting from './components/greeting';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          DAMN SAM GOT A BIG ASS FOREHEAD
-        </p>
+        <Greeting />
+        <p> DAMN SAM GOT A BIG ASS FOREHEAD </p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/greeting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './greeting.css';
+
+function Greeting() {
+  return (
+    <div className = "greeting-container">
+      <h2>Welcome to our Gym Rat Website!</h2>
+      <p className = "greeting-description">
+        Here at our website, you can browse recipes so you can become big and strong like Sam's Forehead!!!
+      </p>
+    </div>
+  );
+}
+
+export default Greeting;
