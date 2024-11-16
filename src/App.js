2b72e235665714c7c2aeffd6ae1640d72e69d578
Maximus Austin <maximus@ucr-secure-36-10-13-194-166.wnet.ucr.edu>
2024-11-16 12:01:11 -0800

    Added more designs to website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
 import Greeting from './components/greeting';
 
-
 function App() {
   return (
     <div className="App">
+      <div className = "navbar">
+        <a href="#home">Home</a>
+        <a href="#recipes">Recipes</a>
+        <a href="#contact">Contact</a>
+      </div>
+
       <header className="App-header">
         <Greeting />
-        <p> DAMN SAM GOT A BIG ASS FOREHEAD </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="button-container">
+          {/* Button for "Find a Recipe" */}
+          <button className="find-recipe-button">
+            Find a Recipe
+          </button>
+          
+          {/* Link for the picture of a cat */}
+          <a
+            className="App-link"
+            href="https://unsplash.com/images/animals/cat"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Here is a picture of a cat :3
+          </a>
+        </div>
       </header>
+
+      {/* Footer */}
+      <div className="footer">
+        <p>© 2024 My Recipe App</p>
+        <a href="https://www.example.com/privacy">Privacy Policy</a>
+      </div>
     </div>
   );
 }
